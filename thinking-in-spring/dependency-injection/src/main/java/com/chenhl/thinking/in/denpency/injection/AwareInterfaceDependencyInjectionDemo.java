d52b118627061c6edf3b4b17b51e65b555d5f308
chenh <chenhonglei@outlook.com>
2020-04-17 23:30:26 +0800

    add aware interface injection demo

---
+++ thinking-in-spring/dependency-injection/src/main/java/com/chenhl/thinking/in/denpency/injection/AwareInterfaceDependencyInjectionDemo.java
@@ -0,0 +1,40 @@
+package com.chenhl.thinking.in.denpency.injection;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AwareInterfaceDependencyInjectionDemo implements BeanFactoryAware, ApplicationContextAware {
+
+    private static BeanFactory beanFactory;
+
+    private static ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+
+        ctx.register(AwareInterfaceDependencyInjectionDemo.class);
+
+        ctx.refresh();
+
+        System.out.println(beanFactory==ctx.getBeanFactory());
+        System.out.println(applicationContext==ctx);
+
+        ctx.close();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        AwareInterfaceDependencyInjectionDemo.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        AwareInterfaceDependencyInjectionDemo.applicationContext = applicationContext;
+    }
+}
